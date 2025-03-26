90f2999d8753bee2a3a16b3cddc55c5a4e6b8274
SJPBP <73006594+SJPBP@users.noreply.github.com>
2025-03-26 18:32:50 -0400

    Renamed autoFormat to conform and changed treesitter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,21 +4,31 @@ return { -- Autoformat
 	cmd = { 'ConformInfo' },
 	keys = {
 		{
-			'<leader>F',
+			'<leader>f',
 			function()
-				require('conform').format { async = true, lsp_format = 'fallback' }
+				require('conform').format {
+					async = true,
+					-- lsp_format = 'fallback'
+				}
 			end,
 			mode = '',
 			desc = '[F]ormat buffer',
 		},
 	},
+
+	-- This will provide type hinting with LuaLS
+	---@module "conform"
+	---@type conform.setupOpts
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = {
+				-- c = true,
+				-- cpp = true
+			}
 			local lsp_format_opt
 			if disable_filetypes[vim.bo[bufnr].filetype] then
 				lsp_format_opt = 'never'
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,4 +2,15 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	"hiphish/rainbow-delimiters.nvim",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			-- ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+			ensure_installed = { "all" },
+			sync_install = false,
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
